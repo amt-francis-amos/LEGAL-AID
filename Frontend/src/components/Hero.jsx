b9fc57791a521a8f218049394bb7ab4303b68916
amt-francis-amos <sugartone45@yahoo.com>
2025-05-27 15:24:17 -0800

    updated

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { motion } from "framer-motion";
 import { FaPhoneAlt, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa";
 import { assets } from "../assets/assets";
