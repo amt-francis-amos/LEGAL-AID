e63a872517dbbc1509c722c9e1dc6b3acefd1ce9
amt-francis-amos <sugartone45@yahoo.com>
2025-05-27 15:27:02 -0800

    updated

--- Frontend/src/pages/TeamMemberDetails.jsx
+++ Frontend/src/pages/TeamMemberDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { useParams, Link } from "react-router-dom";
 import { FaFacebook, FaXTwitter, FaInstagram } from "react-icons/fa6";
 import { teamMembers } from "../assets/assets"; 
